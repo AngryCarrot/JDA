12167daf32b01a07b85fdfa3db57a42b3be7eefe
Haocheng Tang (FA TALENT) <t-hata@microsoft.com>
2017-08-13 14:25:28 +0800

    adjust to fc6

--- run_office.py
+++ run_office.py
@@ -2,51 +2,54 @@ import numpy as np
 import scipy.stats as stats
 import scipy.io as sio
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 
 from JDA import JDA
 
 
 # Set algorithm parameters
-options = {"k": 100, "lmbda": 1.0, "ker": 'linear', "gamma": 1.0}
+options = {"k": 200, "lmbda": 1.0, "ker": 'linear', "gamma": 1.0}
 # options.k = 100
 # options.lambdaP = 1.0
 # options.ker = 'linear'     # 'primal' | 'linear' | 'rbf'
 # options.gamma = 1.0        # kernel bandwidth: rbf only
-T = 10
+T = 20
 
-srcStr = ['Caltech10','Caltech10','Caltech10','amazon','amazon','amazon','webcam','webcam','webcam','dslr','dslr','dslr']
-tgtStr = ['amazon','webcam','dslr','Caltech10','webcam','dslr','Caltech10','amazon','dslr','Caltech10','amazon','webcam']
-
+# srcStr = ['caltech10','caltech10','caltech10','amazon','amazon','amazon','webcam','webcam','webcam','dslr','dslr','dslr']
+# tgtStr = ['amazon','webcam','dslr','caltech10','webcam','dslr','caltech10','amazon','dslr','caltech10','amazon','webcam']
+srcStr = ['caltech10','caltech10','caltech10', 'amazon', 'webcam','dslr']
+tgtStr = ['amazon','webcam','dslr','caltech10','caltech10','caltech10']
+feature_prefix = r"../FFS/{}_{}_vgg_fc6_JDA_{}_feat.npy"
 result = []
 
 for source, target in zip(srcStr, tgtStr):
-    source = srcStr[0]
-    target = tgtStr[0]
     print("{}->{}".format(source, target))
     options["data"] = "{}_vs_{}".format(source, target)
 
     # Source
-    data = sio.loadmat(
-        r"data/{}_SURF_L10.mat".format(source))
-    fts = data["fts"]
+    data = np.load(r"../features/{}_vgg_fc6_feat.npy".format(source))
+    fts = np.array([item["feature"] for item in data])
     # fts = fts ./ repmat(sum(fts,2),1,size(fts,2)); %fts先求每行的和，然后将每行的元素都除以相应行的和
     fts = list(map(lambda item: item / sum(item), fts))
     # Xs = zscore(fts,1);
-    Xs = stats.zscore(fts)
+    # Xs = stats.zscore(fts)
+    mean = np.mean(fts)
+    std = np.std(fts)
+    Xs = (fts - mean) / std
     Xs = Xs.T
-    Ys = data["labels"]
-    Ys = np.array(list(map(lambda item: item[0], Ys)))
+    Ys = np.array([item["label"] for item in data])
 
     # Target
-    data = sio.loadmat(
-        r"data/{}_SURF_L10.mat".format(target))
-    fts = data["fts"]
+    data = np.load(r"../features/{}_vgg_fc6_feat.npy".format(target))
+    fts = np.array([item["feature"] for item in data])
     # fts = fts. / repmat(sum(fts, 2), 1, size(fts, 2));
     fts = list(map(lambda item: item / sum(item), fts))
-    Xt = stats.zscore(fts)
+    # Xt = stats.zscore(fts)
+    mean = np.mean(fts)
+    std = np.std(fts)
+    Xt = (fts - mean) / std
     Xt = Xt.T
-    Yt = data["labels"]
-    Yt = np.array(list(map(lambda item: item[0], Yt)))
+    Yt = np.array([item["label"] for item in data])
 
     print("data prepared!")
 
@@ -54,8 +57,7 @@ for source, target in zip(srcStr, tgtStr):
     nt = Xt.shape[-1]
 
     # soft label
-    # Cls = knnclassify(Xt',Xs',Ys,1);
-    clf = KNeighborsClassifier(n_neighbors=1)
+    clf = LogisticRegression()
     clf.fit(Xs.T, Ys)
 
     Cls = clf.predict(Xt.T)
@@ -65,7 +67,7 @@ for source, target in zip(srcStr, tgtStr):
     Cls = []
     Acc = []
     for t in range(T):
-        print('==============================Iteration {}=============================='.format(t))
+        print('==============================Iteration {} =============================='.format(t))
         Z, A = JDA(Xs, Xt, Ys, Cls, options)
         # Z = Z*diag(sparse(1./sqrt(sum(Z.^2))));
         Z = np.matmul(Z, np.diag(1 / np.sqrt(np.sum(np.square(Z), 0))))
@@ -73,7 +75,8 @@ for source, target in zip(srcStr, tgtStr):
         Zs = Z[:, :ns]
         Zt = Z[:, ns:]
 
-        clf = KNeighborsClassifier(n_neighbors=1)
+
+        clf = LogisticRegression()
         clf.fit(Zs.T, Ys)
 
         Cls = clf.predict(Zt.T)
@@ -81,7 +84,8 @@ for source, target in zip(srcStr, tgtStr):
         acc = len(Cls[Cls == Yt]) / len(Yt)
         print("JDA + NN = {}".format(acc))
         Acc.append(acc)
-
+    np.save(feature_prefix.format(source, target, "source"), {"feature": Zs, "label": Ys})
+    np.save(feature_prefix.format(target, target, "target"), {"feature": Zt, "label": Yt})
 
     result.append(Acc[-1])
     print("\n\n\n")
